a4bc8f2a7f23d48736fa204c30c6d0823e2676a0
lbisson <lbisson@gmail.com>
2016-11-29 22:57:02 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+require('./config/config');
+
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+
+
+
+module.exports = {app};
