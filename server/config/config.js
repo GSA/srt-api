376f6910522a14b03c41a177517819bfb3bf864f
Len Bisson <leonard_bisson@unitedsolutions.biz>
2016-12-14 13:39:11 -0500

    set env for aws

--- server/config/config.js
+++ server/config/config.js
@@ -6,4 +6,7 @@ if (env === 'development') {
 } else if (env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/Section508DashTest'; //modified
+} else {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'lbisson:GSA_SRT@ec2-54-210-59-149.compute-1.amazonaws.com:27017/Section508Dash';
 }
