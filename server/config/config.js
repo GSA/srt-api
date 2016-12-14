a6099a8875de9272951b42b78fb6ce996f633f89
Len Bisson <leonard_bisson@unitedsolutions.biz>
2016-12-14 14:38:27 -0500

    deploy

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,11 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
+// if (env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/Section508Dash'; //modified
+// } else if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/Section508DashTest'; //modified
+// }
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/Section508Dash'; //modified
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/Section508DashTest'; //modified
-}
-  // process.env.PORT = 3000;
-  // process.env.MONGODB_URI = 'SRT:SRT@ec2-54-210-59-149.compute-1.amazonaws.com:27017/Section508Dash';
+  process.env.MONGODB_URI = 'SRT:SRT@ec2-54-210-59-149.compute-1.amazonaws.com:27017/Section508Dash';
