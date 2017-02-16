494fcd628e144eaa1a0bb44266c1b30f6ba4d670
lbisson <lbisson@gmail.com>
2017-02-15 20:48:52 -0500

    set aws mongo

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,11 @@
-var env = process.env.NODE_ENV || 'development';
+// var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/Section508Dash'; //modified
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/Section508DashTest'; //modified
- }
- // process.env.PORT = 3000;
- // process.env.MONGODB_URI = 'srt:srt@ec2-54-145-198-134.compute-1.amazonaws.com:27017/Section508Dash';
+// if (env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/Section508Dash'; //modified
+// } else if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/Section508DashTest'; //modified
+//  }
+ process.env.PORT = 3000;
+ process.env.MONGODB_URI = 'srt:srt@ec2-54-145-198-134.compute-1.amazonaws.com:27017/Section508Dash';
