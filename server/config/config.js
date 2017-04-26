acf84ab857961349d6d375caf0525db322a597cc
lbisson <lbisson@gmail.com>
2017-04-26 13:46:00 -0400

    remove dead file, config for test

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,14 @@
-var env = process.env.NODE_ENV || 'development'; //set env variables for dev or test.
-
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/Section508Dash';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/Section508DashTest';
- }
+// var env = process.env.NODE_ENV || 'development'; //set env variables for dev or test.
+//
+// if (env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/Section508Dash';
+// } else if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/Section508DashTest';
+//  }
 
  // Below two lines are the AWS Dev/Demo environment
  // TODO: add production to ENV configuration so we don't have to comment or uncomment lines
- // process.env.PORT = 3000;
- // process.env.MONGODB_URI = 'srt:srt@ec2-54-145-198-134.compute-1.amazonaws.com:27017/Section508Dash';
+ process.env.PORT = 3000;
+ process.env.MONGODB_URI = 'srt:srt@ec2-54-145-198-134.compute-1.amazonaws.com:27017/Section508Dash';
