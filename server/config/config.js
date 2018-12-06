7717eddfa72035e5ca293e425c4ec38c0c2d4dd2
Al Crowley <albert.crowley@tcg.com>
2018-12-06 18:36:50 +0000

    Working on cloud.gov

--- server/config/config.js
+++ server/config/config.js
@@ -2,12 +2,12 @@
 // Below two lines are the AWS Dev/Demo environment (developement Envirnoment)
 // TODO: add production to ENV configuration so we don't have to comment or uncomment lines
 // productionURL
-process.env.PORT = 3000;
+process.env.PORT = 8080;
 // process.env.MONGODB_URI = 'srt:srt@localhost:27017/Section508Dash';
 
 // localdev
-process.env.MONGODB_URI = 'localhost:27017/Section508Dash';
+// process.env.MONGODB_URI = 'localhost:27017/Section508Dash';
 
 // cloud.gov
-//process.env.MONGODB_URI = ZJJ01kWhWX:***REMOVED***@xa2b82717666a4-master-client.service.kubernetes:31473/9rTP2N2npx
+process.env.MONGODB_URI = 'mongodb://ZJJ01kWhWX:***REMOVED***@xa2b82717666a4-master-client.service.kubernetes:31473/9rTP2N2npx';
 
