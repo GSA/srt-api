121680f11e40a44e71b39dea2bff7b7ad63b3a52
Al Crowley <albert.crowley@tcg.com>
2018-12-20 23:20:10 -0500

    passes unit tests

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ process.env.MONGODB_URI = 'localhost:27017/Section508Dash';
 process.env.POSTGRES_UIR = 'postgres://utz6kah1zq8t2nyg:***REMOVED***@192.168.1.171:5432/cgawsbrokerprodgq3wrvn8c8a772q';
 process.env.POSTGRES_DB = 'cgawsbrokerprodgq3wrvn8c8a772q';
 process.env.POSTGRES_PORT = '5432';
-process.env.POSTGRES_HOST = '192.168.1.171';
+process.env.POSTGRES_HOST = '192.168.1.130';
 process.env.POSTGRES_PASSWORD = '***REMOVED***';
 process.env.POSTGRES_USER = 'utz6kah1zq8t2nyg';
 
