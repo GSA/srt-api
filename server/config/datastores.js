a5478f471c81352e284dd8409fb45054383e32d9
Al Crowley <albert.crowley@tcg.com>
2019-01-08 09:12:46 -0500

    Adding code coverage settings

--- server/config/datastores.js
+++ server/config/datastores.js
@@ -1,25 +1,25 @@
-
-const Waterline = require('waterline');
-const config = require('../config/environment');
-const orm = new Waterline();
-
-
-var fs = require('fs');
-var path = require('path');
-
-
-
-fs
-    .readdirSync(__dirname)
-    .filter(function(file) {
-        return file.indexOf('.') === -1;
-    })
-    .forEach(function(file) {
-        var modelPath = path.join(__dirname, file, file + '.model.js');
-
-        if(fs.existsSync(modelPath)) {
-            orm.loadCollection(require(modelPath));
-        }
-    });
-
-module.exports = {waterline: orm, config: config.waterline};
+//
+// const Waterline = require('waterline');
+// const config = require('../config/environment');
+// const orm = new Waterline();
+//
+//
+// var fs = require('fs');
+// var path = require('path');
+//
+//
+//
+// fs
+//     .readdirSync(__dirname)
+//     .filter(function(file) {
+//         return file.indexOf('.') === -1;
+//     })
+//     .forEach(function(file) {
+//         var modelPath = path.join(__dirname, file, file + '.model.js');
+//
+//         if(fs.existsSync(modelPath)) {
+//             orm.loadCollection(require(modelPath));
+//         }
+//     });
+//
+// module.exports = {waterline: orm, config: config.waterline};
