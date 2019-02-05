592cb91ad8e456a0e4641d31503a35d7707e67eb
Al Crowley <albert.crowley@tcg.com>
2019-02-05 08:47:21 -0500

    adding code to test CI database setup

--- server/migrations/20190126000000-add-timestamps.js
+++ server/migrations/20190126000000-add-timestamps.js
@@ -4,7 +4,15 @@
 module.exports = {
     up: (queryInterface, Sequelize) => {
 
-        return queryInterface.addColumn("notice", "createdAt", {type: "timestamptz", defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')})
+        let sql = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type LIKE '%TABLE' AND table_name != 'spatial_ref_sys'";
+
+        return  Sequelize.query(sql, {type: db.sequelize.QueryTypes.SELECT})
+            .then ( tables => {
+                console.log(tables);
+            })
+            .then ( () => {
+                return queryInterface.addColumn("notice", "createdAt", {type: "timestamptz", defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')})
+            })
             .then ( () => {
                 return queryInterface.addColumn("notice", "updatedAt", {type: "timestamptz", defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')});
             })
