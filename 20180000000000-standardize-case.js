aadfcdb8a0dea3cb53b862966fa64e38509bbb47
Al Crowley <albert.crowley@tcg.com>
2019-02-07 10:08:00 -0500

    Try using pure SQL for migrations of legacy tables

---
+++ 20180000000000-standardize-case.js
@@ -0,0 +1,115 @@
+'use strict';
+
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const pg = require ('pg');
+
+var connectionString = "postgres://" + config.username + ":" +config.password+ "@"+ config.host+":" + config.port +"/" + config.database;
+
+var pgClient = new pg.Client(connectionString);
+
+
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return pgClient.connect()
+            .then( () => {
+                var sql = "BEGIN"
+                console.log (sql)
+                return pgClient.query(sql)
+                    // .then( () => {
+                    //     var sql = 'select * from '+config.database+'.notice limit 1';
+                    //     console.log (sql)
+                    //     return pgClient.query(sql)
+                    // })
+                    // .then( (result) => {
+                    //     console.log(result);
+                    //     var sql = 'alter table "notice"  rename to "Notice"';
+                    //     console.log (sql)
+                    //     return pgClient.query(sql)
+                    // })
+                    // .then( () => {
+                    //     var sql = 'alter table "notice_type"  rename to "Notice_Type"';
+                    //     console.log (sql)
+                    //     return pgClient.query(sql)
+                    // })
+                    // .then( () => {
+                    //     var sql = 'alter table "attachment"  rename to "Attachment"';
+                    //     console.log (sql)
+                    //     return pgClient.query(sql)
+                    // })
+                    // .then( () => {
+                    //     var sql = 'alter table "model"  rename to "Model"';
+                    //     console.log (sql)
+                    //     return pgClient.query(sql)
+                    // })
+                    .then( () => {
+                        var sql = 'COMMIT';
+                        console.log (sql)
+                        return pgClient.query(sql)
+                    })
+            })
+            .then ( () => {
+                pgClient.end();
+            })
+            .catch( (e) => {
+                var sql = 'ROLLBACK';
+                console.log (sql)
+                return pgClient.query(sql)
+                    .then ( () => {
+                        throw e;
+                    })
+            })
+    },
+    down: (queryInterface, Sequelize) => {
+        return pgClient.connect()
+            .then( () => {
+                var sql = "BEGIN"
+                console.log (sql)
+                return pgClient.query(sql)
+                    // .then( () => {
+                    //     var sql = 'select * from notice limit 1';
+                    //     console.log (sql)
+                    //     return pgClient.query(sql)
+                    // })
+                    // .then( (result) => {
+                    //     console.log(result);
+                    //     var sql = 'alter table "Notice"  rename to "notice"';
+                    //     console.log (sql)
+                    //     return pgClient.query(sql)
+                    // })
+                    // .then( () => {
+                    //     var sql = 'alter table "Attachment"  rename to "attachment"';
+                    //     console.log (sql)
+                    //     return pgClient.query(sql)
+                    // })
+                    // .then( () => {
+                    //     var sql = 'alter table "Notice_Type"  rename to "notice_type"';
+                    //     console.log (sql)
+                    //     return pgClient.query(sql)
+                    // })
+                    // .then( () => {
+                    //     var sql = 'alter table "Model"  rename to "model"';
+                    //     console.log (sql)
+                    //     return pgClient.query(sql)
+                    // })
+                    .then( () => {
+                        var sql = 'COMMIT';
+                        console.log (sql)
+                        return pgClient.query(sql)
+                    })
+            })
+            .then ( () => {
+                pgClient.end();
+            })
+            .catch( (e) => {
+                console.log (e)
+                var sql = 'ROLLBACK';
+                console.log (sql)
+                return pgClient.query(sql)
+                    .then ( () => {
+                        throw e;
+                    })
+            })
+    }
+};
--- server/migrations/20190126000000-add-timestamps.js
+++ server/migrations/20190126000000-add-timestamps.js
@@ -1,42 +1,101 @@
 'use strict';
 
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const pg = require ('pg');
+var connectionString = "postgres://" + config.username + ":" +config.password+ "@"+ config.host+":" + config.port +"/" + config.database;
+var pgClient = new pg.Client(connectionString);
+
+let up_sql = [
+    'BEGIN',
+    'alter table "notice" add column "createdAt" timestamp default now(), add column "updatedAt" timestamp default now()',
+    'alter table "notice_type" add column "createdAt" timestamp default now(), add column "updatedAt" timestamp default now()',
+    'alter table "attachment" add column "createdAt" timestamp default now(), add column "updatedAt" timestamp default now()',
+    'COMMIT'
+];
+
+let down_sql =[
+    'BEGIN',
+    'alter table "notice" drop column "createdAt" , drop column "updatedAt" ',
+    'alter table "notice_type" drop column "createdAt" , drop column "updatedAt" ',
+    'alter table "attachment" drop column "createdAt" , drop column "updatedAt" ',
+    'COMMIT'
+];
+let increment = 0;
+
+function runNext (sql) {
+    let promise = null;
+    if (increment < sql.length) {
+        console.log (sql[increment]);
+        promise = pgClient.query(sql[increment]);
+    }
+    increment++;
+    return promise;
+}
 
 module.exports = {
-    up: (queryInterface, Sequelize) => {
-        return queryInterface.addColumn("notice", "createdAt", {type: "timestamptz", defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')})
-            .then ( () => {
-                return queryInterface.addColumn("notice", "updatedAt", {type: "timestamptz", defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')});
-            })
-            .then ( () => {
-                return queryInterface.addColumn("notice_type", "createdAt", {type: "timestamptz", defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')});
-            })
-            .then ( () => {
-                return queryInterface.addColumn("notice_type", "updatedAt", {type: "timestamptz", defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')});
-            })
-            .then ( () => {
-                return queryInterface.addColumn("attachment", "createdAt", {type: "timestamptz", defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')});
-            })
-            .then ( () => {
-                return queryInterface.addColumn("attachment", "updatedAt", {type: "timestamptz", defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')});
-            })
+    up: async (queryInterface, Sequelize) => {
+        await pgClient.connect();
+        try {
+            while (increment < up_sql.length) {
+                await runNext(up_sql).catch( e => {throw e});
+            }
+            return;
+        } catch (e) {
+            console.log ("ROLLBACK");
+            console.log (e);
+            return pgClient.query("ROLLBACK")
+                .then ( () => {throw e});
+        }
+
+        // return queryInterface.addColumn("notice", "createdAt", {type: "timestamptz", defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')})
+        //     .then ( () => {
+        //         return queryInterface.addColumn("notice", "updatedAt", {type: "timestamptz", defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')});
+        //     })
+        //     .then ( () => {
+        //         return queryInterface.addColumn("notice_type", "createdAt", {type: "timestamptz", defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')});
+        //     })
+        //     .then ( () => {
+        //         return queryInterface.addColumn("notice_type", "updatedAt", {type: "timestamptz", defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')});
+        //     })
+        //     .then ( () => {
+        //         return queryInterface.addColumn("attachment", "createdAt", {type: "timestamptz", defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')});
+        //     })
+        //     .then ( () => {
+        //         return queryInterface.addColumn("attachment", "updatedAt", {type: "timestamptz", defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')});
+        //     })
     },
-    down: (queryInterface, Sequelize) => {
-        return queryInterface.removeColumn("notice", "createdAt")
-            .then( () => {
-                return queryInterface.removeColumn("notice", "updatedAt")
-            })
-            .then( () => {
-                return queryInterface.removeColumn("notice_type", "createdAt")
-            })
-            .then( () => {
-                return queryInterface.removeColumn("notice_type", "updatedAt")
-            })
-            .then( () => {
-                return queryInterface.removeColumn("attachment", "createdAt")
-            })
-            .then( () => {
-                return queryInterface.removeColumn("attachment", "updatedAt")
-            })
+    down: async (queryInterface, Sequelize) => {
+        await pgClient.connect();
+        try {
+            while (increment < down_sql.length) {
+                await runNext(down_sql).catch( e => {throw e});
+            }
+            return;
+        } catch (e) {
+            console.log ("ROLLBACK");
+            console.log (e);
+            return pgClient.query("ROLLBACK")
+                .then ( () => {throw e});
+
+        }
+
+        // return queryInterface.dropColumn("notice", "createdAt")
+        //     .then( () => {
+        //         return queryInterface.dropColumn("notice", "updatedAt")
+        //     })
+        //     .then( () => {
+        //         return queryInterface.dropColumn("notice_type", "createdAt")
+        //     })
+        //     .then( () => {
+        //         return queryInterface.dropColumn("notice_type", "updatedAt")
+        //     })
+        //     .then( () => {
+        //         return queryInterface.dropColumn("attachment", "createdAt")
+        //     })
+        //     .then( () => {
+        //         return queryInterface.dropColumn("attachment", "updatedAt")
+        //     })
 
     }
 };
