1994717c7f287870f26c22480caf6083fb1d3994
Al Crowley <albert.crowley@tcg.com>
2019-04-20 08:08:27 -0400

    pointing sequelize to the config.js where we can read environment vars. (#19)

--- server/config/config.js
+++ server/config/config.js
@@ -1,22 +1,30 @@
+// export DB connection for sequelize command line
+// you can run it with:
+// node ./node_modules/.bin/sequelize -c server/config/config.js
+let config = require('./config.json')
+
+if (process.env.VCAP_SERVICES) {
+  // noinspection SpellCheckingInspection
+  const VCAP = JSON.parse(process.env.VCAP_SERVICES)
+
+  const dbSettings = {
+    database: VCAP['aws-rds'][0]['credentials']['db_name'],
+    username: VCAP['aws-rds'][0]['credentials']['username'],
+    password: VCAP['aws-rds'][0]['credentials']['password'],
+    host: VCAP['aws-rds'][0]['credentials']['host'],
+    port: VCAP['aws-rds'][0]['credentials']['port'],
+    dialect: 'postgres',
+    logging: false
+  }
+
+  config= {
+    development: dbSettings
+  }
+
+  if (process.env.NODE_ENV) {
+    config[process.env.NODE_ENV] = dbSettings
+  }
+}
 
 
-// Below two lines are the AWS Dev/Demo environment (developement Envirnoment)
-// TODO: add production to ENV configuration so we don't have to comment or uncomment lines
-// productionURL
-process.env.PORT = 3000;
-// process.env.MONGODB_URI = 'srt:srt@localhost:27017/Section508Dash';
-
-// localdev
-process.env.MONGODB_URI = 'localhost:27017/Section508Dash';
-
-// process.env.POSTGRES_UIR = 'postgres://utz6kah1zq8t2nyg:***REMOVED***@cg-aws-broker-prod-01fb6e1ed561.ci7nkegdizyy.us-gov-west-1.rds.amazonaws.com:5432/cgawsbrokerprodgq3wrvn8c8a772q';
-// process.env.POSTGRES_UIR = 'postgres://utz6kah1zq8t2nyg:***REMOVED***@192.168.1.171:5432/cgawsbrokerprodgq3wrvn8c8a772q';
-// process.env.POSTGRES_DB = 'cgawsbrokerprodgq3wrvn8c8a772q';
-// process.env.POSTGRES_PORT = '5432';
-// process.env.POSTGRES_HOST = '192.168.1.130';
-// process.env.POSTGRES_PASSWORD = '***REMOVED***';
-// process.env.POSTGRES_USER = 'utz6kah1zq8t2nyg';
-
-// cloud.gov
-// process.env.MONGODB_URI = ZJJ01kWhWX:***REMOVED***@xa2b82717666a4-master-client.service.kubernetes:31473/9rTP2N2npx
-
+module.exports = config
