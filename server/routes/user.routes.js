e76454e8b51eeb3e7e762ffb5447eb279f79e4d8
Al Crowley <albert.crowley@tcg.com>
2020-02-28 16:52:45 -0500

    Add agency translation to Masquarade route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -181,7 +181,7 @@ module.exports = {
     }
 
     let decoded = jwt.decode(req.headers['authorization'].split(' ')[1])      /** @namespace decoded.user*/
-    decoded.user.agency = req.query.agency
+    decoded.user.agency = authRoutes.translateCASAgencyName(req.query.agency)
     decoded.user.userRole = req.query.role
     decoded.user.grouplist = authRoutes.roleNameToCASGroup(req.query.role)
     decoded.user.firstName = decoded.user.firstName + " M"
