de95895d23c67281598d681cbd1e39ddf64bd165
Al Crowley <albert.crowley@tcg.com>
2020-04-23 15:57:12 -0400

    updating npm  packages to fix vulnerabilities (#74)

--- server/models/create_models.sh
+++ server/models/create_models.sh
@@ -9,7 +9,7 @@ sequelize model:generate --force --name Agency --attributes agency:string,acrony
 sequelize model:generate --force --name Survey --attributes id:integer,question:string,choices:jsonb,section:string,type:string,answer:string,Note:string,choicesNote:jsonb
 
 # other table
-npm install sequelize-auto
+npm install sequelize-auto-v2
 npm install pg@6.4.2
 sequelize-auto -s public -e postgres -h 192.168.1.126 -d cgawsbrokerprodgq3wrvn8c8a772q -u utz6kah1zq8t2nyg -x ***REMOVED***
 ***REMOVED***  -o .\server\models -t attachment
