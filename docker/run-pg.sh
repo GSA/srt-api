e861efa5dee8151e0e1215c3d2a6ee03e6baab91
BuckinghamAJ <ajbuckingham7@gmail.com>
2023-05-10 13:53:07 -0400

    Think we should use newer postgres image

--- docker/run-pg.sh
+++ docker/run-pg.sh
@@ -2,8 +2,8 @@
 
 sudo docker stop srt-db
 sudo docker rm srt-db
-echo "Starting Postgres version 9.5.15"
-sudo docker run --name srt-db -e POSTGRES_DB=srt -e POSTGRES_USER=circleci -e POSTGRES_PASSWORD=srtpass -v $PWD/sql:/docker-entrypoint-initdb.d -p 5432:5432 --restart unless-stopped -d postgres:9.5.15
+echo "Starting Postgres version"
+sudo docker run --name srt-db -e POSTGRES_DB=srt -e POSTGRES_USER=circleci -e POSTGRES_PASSWORD=srtpass -v $PWD/sql:/docker-entrypoint-initdb.d -p 5432:5432 --restart unless-stopped -d postgres
 while ! curl http://localhost:5432/ 2>&1 | grep '52'
 do
   sleep 1
