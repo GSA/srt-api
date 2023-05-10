fadc2421664d511a0a5d9cdd7f58bf6978c9b309
BuckinghamAJ <ajbuckingham7@gmail.com>
2023-05-10 13:37:46 -0400

    sync before app starts to verify models match

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const env = process.env.NODE_ENV || 'development'
 const config = require('./config/config.js')[env]
 const app = require('./app')()
+const db = require('./models')
 // noinspection JSUnresolvedVariable
 const port = config.srt_server.port
 
-app.listen(port, '0.0.0.0',() => {
-  console.log(`Started up at port ${port}`) // allowed output
-})
+
+db.sequelize.sync().then(() => {
+  app.listen(port, '0.0.0.0',() => {
+    console.log(`Started up at port ${port}`) // allowed output
+  })
+});
 
 module.exports = app
