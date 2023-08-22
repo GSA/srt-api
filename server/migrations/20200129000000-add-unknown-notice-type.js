e0768d280ddfd0c49c8e6701c261aab20e85762f
Adam Buckingham <AJBuckingham7@gmail.com>
2023-08-22 13:50:29 -0400

    Need to have some createdAt value to work

--- server/migrations/20200129000000-add-unknown-notice-type.js
+++ server/migrations/20200129000000-add-unknown-notice-type.js
@@ -4,7 +4,7 @@ const migrationUtils = require('../migrationUtil')
 // This migration is to force a rebuild of the Predictions table
 
 let upSql = [
-  "insert into notice_type values (0, 'Unknown')"
+  "insert into notice_type values (0, 'Unknown', '2023-08-21 13:24:18.24947')"
 ]
 
 let downSql = [
